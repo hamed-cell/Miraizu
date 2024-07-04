0e36064c27cc3cc01b111ebc79559789b126ab81
Thien Nguyen <thien.ngu25@gmail.com>
2024-07-04 10:50:23 +0200

    ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import Logo from "./assets/logo.png";
+import "./App.css";
 
-const App = () => {
+function App() {
   return (
     <div className="container">
       <header className="app-bar">
-        <Link to="/">ミライズ (MiraiZu)</Link>
+        <Link to="/">
+          <img className="logo" src={Logo} alt="" />
+        </Link>
       </header>
       <main>
         <Outlet />
       </main>
     </div>
   );
-};
+}
 
 export default App;
