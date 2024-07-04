07d9b2d8b34488582293dd4eceeb8fb16bb15162
Attar Hamed <attar30pro@gmail.com>
2024-07-04 10:46:45 +0200

    searchbar

--- frontend/src/components/CompanyList.jsx
+++ frontend/src/components/CompanyList.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { companies } from '../data/companies';
+import { companies as companiesData } from '../data/companies';
+import SearchBar from './SearchBar';
 import '../App.css';
 
 const getAverageScore = (reviews) => {
@@ -11,22 +12,27 @@ const getAverageScore = (reviews) => {
 const CompanyList = () => {
   const [sortOrder, setSortOrder] = useState('asc');
   const [sortCategory, setSortCategory] = useState('average');
+  const [filteredCompanies, setFilteredCompanies] = useState(companiesData);
 
-  const sortedCompanies = [...companies].sort((a, b) => {
-    let avgA, avgB;
-    if (sortCategory === 'average') {
-      avgA = parseFloat(getAverageScore(a.reviews));
-      avgB = parseFloat(getAverageScore(b.reviews));
-    } else {
-      avgA = a.reviews.find(review => review.category === sortCategory)?.rating || 0;
-      avgB = b.reviews.find(review => review.category === sortCategory)?.rating || 0;
-    }
-    return sortOrder === 'asc' ? avgA - avgB : avgB - avgA;
-  });
+  useEffect(() => {
+    const sortedCompanies = [...filteredCompanies].sort((a, b) => {
+      let avgA, avgB;
+      if (sortCategory === 'average') {
+        avgA = parseFloat(getAverageScore(a.reviews));
+        avgB = parseFloat(getAverageScore(b.reviews));
+      } else {
+        avgA = a.reviews.find(review => review.category === sortCategory)?.rating || 0;
+        avgB = b.reviews.find(review => review.category === sortCategory)?.rating || 0;
+      }
+      return sortOrder === 'asc' ? avgA - avgB : avgB - avgA;
+    });
+    setFilteredCompanies(sortedCompanies);
+  }, [sortOrder, sortCategory, filteredCompanies]);
 
   return (
     <div className="paper">
       <h2>Liste des entreprises</h2>
+      <SearchBar companies={companiesData} setFilteredCompanies={setFilteredCompanies} />
       <div className="form-group">
         <div className="form-control">
           <label>Trier par</label>
@@ -45,7 +51,7 @@ const CompanyList = () => {
         </div>
       </div>
       <div>
-        {sortedCompanies.map((company) => (
+        {filteredCompanies.map((company) => (
           <div key={company.id} className="card">
             <Link to={`/company/${company.id}`}>
               <h2>{company.name}</h2>
