5a44c664c34f9080a874d9adab7dad29ee7f3af4
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-07-04 10:47:11 +0200

    add css

--- frontend/src/pages/Contact/Formulaire.jsx
+++ frontend/src/pages/Contact/Formulaire.jsx
@@ -30,98 +30,100 @@ export default function Formulaire() {
 
   return (
     <div>
-      <div className={styles.formulaireContact}>
-        <h3>Informations de contact</h3>
-        <p>
-          Vous êtes un professsionnel et vous souhaitez appraitre sur notre sute
-          ?
-        </p>
-        <img src={Phone} alt="phone logo" />
-        <p>+10123456 789</p>
-        <img src={Email} alt="email logo" />
-        <p>miraizu@gmail.com</p>
-        <img src={Location} alt="location logo" />
-        <p>132 dartmouth street Boston,Massachusetts 02156 United States</p>
-        <div className={styles.imageContainer}>
-          <img src={Twitter} alt="twitter logo" />
-          <img src={Instagram} alt="instagram logo" />
-          <img src={Discord} alt="discord logo" />
+      <div className={styles.contactContainerAll}>
+        <div className={styles.formulaireContact}>
+          <h3>Informations de contact</h3>
+          <p>
+            Vous êtes un professsionnel et vous souhaitez appraitre sur notre
+            sute ?
+          </p>
+          <img src={Phone} alt="phone logo" />
+          <p>+10123456 789</p>
+          <img src={Email} alt="email logo" />
+          <p>miraizu@gmail.com</p>
+          <img src={Location} alt="location logo" />
+          <p>132 dartmouth street Boston,Massachusetts 02156 United States</p>
+          <div className={styles.imageContainer}>
+            <img src={Twitter} alt="twitter logo" />
+            <img src={Instagram} alt="instagram logo" />
+            <img src={Discord} alt="discord logo" />
+          </div>
         </div>
-      </div>
 
-      <Form
-        method="post"
-        className={styles.contactForm}
-        onSubmit={handleSubmit}
-      >
-        <label htmlFor="prènom" className={styles.rowFormRow}>
-          <p className={styles.titleForm}>Prènom</p>
-        </label>
-        <div className={styles.pseudoInput}>
-          <input
-            className={styles.textInput}
-            type="text"
-            placeholder="Taro"
-            name="prenom"
-            value={values.prenom}
-            onChange={handleInput}
-          />
-        </div>
-        <label htmlFor="nom" className={styles.rowFormRow}>
-          <p className={styles.titleForm}>Nom</p>
-        </label>
-        <div className={styles.pseudoInput}>
-          <input
-            className={styles.textInput}
-            type="text"
-            placeholder="miraizu"
-            name="nom"
-            value={values.nom}
-            onChange={handleInput}
-          />
-        </div>
-        <label htmlFor="email" className={styles.rowFormRow}>
-          <p className={styles.titleForm}>Adresse email</p>
-        </label>
-        <div className={styles.pseudoInput}>
-          <input
-            type="email"
-            placeholder="taro.mail@gmail.com"
-            name="email"
-            value={values.email}
-            onChange={handleInput}
-          />
-        </div>
-        <label htmlFor="numero" className={styles.rowFormRow}>
-          <p className={styles.titleForm}>Numéro de téléphone</p>
-        </label>
-        <div className={styles.pseudoInput}>
-          <input
-            type="numero"
-            placeholder="12345678"
-            name="numero"
-            value={values.numero}
-            onChange={handleInput}
-          />
-        </div>
-        <label htmlFor="commentaire" className={styles.rowFormRow}>
-          <p className={styles.titleForm}>commentaire</p>
-        </label>
-        <div className={styles.pseudoInput}>
-          <input
-            type="text"
-            placeholder="saisissez votre message"
-            name="message"
-            value={values.message}
-            onChange={handleInput}
-          />
-        </div>
-        <div className={styles.contactButton}>
-          <button className={styles.buttonSubmit} type="submit">
-            <p className={styles.inscriptionButton}>Envoyer votre message</p>
-          </button>
-        </div>
-      </Form>
+        <Form
+          method="post"
+          className={styles.contactForm}
+          onSubmit={handleSubmit}
+        >
+          <label htmlFor="prènom" className={styles.rowFormRow}>
+            <p className={styles.titleForm}>Prènom</p>
+          </label>
+          <div className={styles.pseudoInput}>
+            <input
+              className={styles.textInput}
+              type="text"
+              placeholder="Taro"
+              name="prenom"
+              value={values.prenom}
+              onChange={handleInput}
+            />
+          </div>
+          <label htmlFor="nom" className={styles.rowFormRow}>
+            <p className={styles.titleForm}>Nom</p>
+          </label>
+          <div className={styles.pseudoInput}>
+            <input
+              className={styles.textInput}
+              type="text"
+              placeholder="miraizu"
+              name="nom"
+              value={values.nom}
+              onChange={handleInput}
+            />
+          </div>
+          <label htmlFor="email" className={styles.rowFormRow}>
+            <p className={styles.titleForm}>Adresse email</p>
+          </label>
+          <div className={styles.pseudoInput}>
+            <input
+              type="email"
+              placeholder="taro.mail@gmail.com"
+              name="email"
+              value={values.email}
+              onChange={handleInput}
+            />
+          </div>
+          <label htmlFor="numero" className={styles.rowFormRow}>
+            <p className={styles.titleForm}>Numéro de téléphone</p>
+          </label>
+          <div className={styles.pseudoInput}>
+            <input
+              type="numero"
+              placeholder="12345678"
+              name="numero"
+              value={values.numero}
+              onChange={handleInput}
+            />
+          </div>
+          <label htmlFor="commentaire" className={styles.rowFormRow}>
+            <p className={styles.titleForm}>commentaire</p>
+          </label>
+          <div className={styles.pseudoInput}>
+            <input
+              type="text"
+              placeholder="saisissez votre message"
+              name="message"
+              value={values.message}
+              onChange={handleInput}
+            />
+          </div>
+          <div className={styles.contactButton}>
+            <button className={styles.buttonSubmit} type="submit">
+              <p className={styles.inscriptionButton}>Envoyer votre message</p>
+            </button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
